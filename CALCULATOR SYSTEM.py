73b97fa055ed3442b4157a703d04694fab25beb3
adityaghosh5310 <47422828+adityaghosh5310@users.noreply.github.com>
2021-07-20 15:46:09 +0530

    Add files via upload

---
+++ CALCULATOR SYSTEM.py
@@ -0,0 +1,13 @@
+n=int(input("Enter 1st no."))
+m=int(input("Enter 2nd no."))
+print("Enter your choice")
+uc=int(input("Enter 1 for sum, 2 for mul, 3 for div, 4 for sub"))
+if(uc==1):
+    print(n+m)
+elif(uc==2):
+    print(n*m)
+elif(uc==3):
+    print(n/m)
+elif(uc==4):
+    print(n-m)
+    
