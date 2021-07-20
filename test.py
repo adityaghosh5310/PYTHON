955f5faaa367dec17cb935a2bd11980ae22ab15e
adityaghosh5310 <47422828+adityaghosh5310@users.noreply.github.com>
2021-07-20 11:57:23 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+a=int(input("enter the no."))
+if(a>=91 and a<=100):
+    print('O')
+elif(a>=81 and a<=90):
+    print('E')
+elif(a>=71 and a<=80):
+    print('A')
+elif(a>=61 and a<=70):
+    print('B')
+elif(a>=51 and a<=60):
+    print('C')
+elif(a>=40 and a<=50):
+    print('D')
+else:
+    print('F')
