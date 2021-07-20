6f20786088152af102ba8f32d4834340fe109af3
adityaghosh5310 <47422828+adityaghosh5310@users.noreply.github.com>
2021-07-20 16:40:50 +0530

    Add files via upload

---
+++ prime in range.py
@@ -0,0 +1,10 @@
+m = int(input("Enter lower range: "))  
+n = int(input("Enter upper range: "))  
+  
+for i in range(m,n + 1):  
+   if i > 1:  
+       for j in range(2,i):  
+           if (i % j) == 0:  
+               break  
+       else:  
+           print(i)  
